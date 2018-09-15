02c44ba6f2a0cc7be4d00cd672f174d540e38174
Antonis Kalou <antonis.kalou@zivver.com>
2018-09-15 18:01:13 +0200

    Extract UI and event handling

--- src/main/scala/com/kalouantonis/channel/ChannelApp.scala
+++ src/main/scala/com/kalouantonis/channel/ChannelApp.scala
@@ -2,31 +2,52 @@ package com.kalouantonis.channel
 
 import java.io.File
 
-import scala.collection.JavaConverters._
-
-import javafx.application.Application
+import javafx.application.{Application, Platform}
 import javafx.beans.value.ObservableValue
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import javafx.stage.Stage
 import javafx.scene.{Parent, Scene}
 import javafx.scene.control.{Button, TableColumn, TableView, TableRow}
-import javafx.scene.input.MouseButton
+import javafx.scene.input.{MouseButton, MouseEvent}
 import javafx.scene.layout.{HBox, VBox}
+
+import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext
 
 import cats.effect.IO
 import com.kalouantonis.channel.media.Track
+import monix.execution.Scheduler
+import monix.reactive.Observable
+import monix.execution.Cancelable
+import monix.execution.cancelables.SingleAssignmentCancelable
+import monix.reactive.Observable
+import monix.reactive.OverflowStrategy.Unbounded
+
+object schedulers {
+  val JavaFx = Scheduler(
+    new ExecutionContext {
+      override def execute(runnable: Runnable): Unit =
+        Platform.runLater(runnable)
+
+      override def reportFailure(t: Throwable): Unit =
+        sys.error(t.getMessage())
+    })
+
+  object Implicits {
+    implicit val JavaFx = schedulers.JavaFx
+  }
+}
 
 object Views {
-  def trackListView(tracks: Seq[Track]): TableView[Track] = {
-    val table = new TableView[Track]()
-    table.getColumns.addAll(
+  def trackList(tracks: Seq[Track]): TableView[Track] = {
+    val delegate = new TableView[Track]()
+    delegate.getColumns.addAll(
       column("Title", track => new SimpleStringProperty(track.title)),
       column("Album", track => new SimpleStringProperty(track.album)),
       column("Artist", track => new SimpleStringProperty(track.artist)))
-    table.setEditable(false)
-    table.setItems(FXCollections.observableArrayList(tracks.asJava))
-    table
+    delegate.setEditable(false)
+    delegate
   }
 
   private def column[Item, T](
@@ -39,11 +60,13 @@ object Views {
   }
 }
 
+object Observables {
+}
+
 // NOTE
 //
-// No one until fucking JAVA 9!!!!! decided to allow starting
-// an application by passing it NORMAL parameters, so we use this
-// as the entry point.
+// Jdk8 does not support starting an application by passing it NORMAL parameters,
+// so we use this as the entry point for now.
 //
 // On the plus side, it makes lifecycle management a bit simpler in regards
 // to running on a generic "platform" (e.g. mobile or desktop).
@@ -59,7 +82,7 @@ final class ChannelApp extends Application {
   override def start(primaryStage: Stage): Unit = {
     // TODO
     val config = ChannelApp.loadConfig().unsafeRunSync()
-    val root = loadUI(primaryStage, config).unsafeRunSync()
+    val root = loadUI(primaryStage, config)
     primaryStage.setTitle(FullAppName)
     primaryStage.setWidth(1260)
     primaryStage.setHeight(860)
@@ -67,22 +90,45 @@ final class ChannelApp extends Application {
     primaryStage.show()
   }
 
-  def loadUI(primaryStage: Stage, config: Config): IO[Parent] = IO {
-    val availableListView = Views.trackListView(availableTracks)
-    availableListView.setRowFactory { _ =>
-      val row = new TableRow[Track]
-      row.setOnMouseClicked { ev =>
-        if (ev.getButton == MouseButton.PRIMARY && ev.getClickCount == 2) {
-          println(s"Double clicked: ${row.getItem}")
+  def tableSelections(
+      table: TableView[Track])(
+      implicit scheduler: Scheduler
+  ): Observable[Track] =
+    Observable.create(Unbounded) { subscriber =>
+      def isDoubleClick(ev: MouseEvent): Boolean =
+        ev.getButton == MouseButton.PRIMARY && ev.getClickCount == 2
+
+      val c = SingleAssignmentCancelable()
+      table.setRowFactory { _ =>
+        val row = new TableRow[Track]
+        // TODO: should I keep track of handlers?
+        row.setOnMouseClicked { ev =>
+          if (isDoubleClick(ev) && row.getItem != null) {
+            subscriber
+              .onNext(row.getItem)
+              .syncOnStopOrFailure(_ => c.cancel())
+          }
         }
+        row
       }
-      row
+      c := Cancelable(() => table.setRowFactory(null))
     }
 
+  def loadUI(primaryStage: Stage, config: Config): Parent = {
+    val availableListView = Views.trackList(availableTracks)
+    val queuedListView = Views.trackList(queuedTracks)
+
+    implicit val jfxScheduler = schedulers.JavaFx
+    tableSelections(availableListView)
+      .dump("O")
+      .subscribe()
+
+    availableListView.setItems(
+      FXCollections.observableList(availableTracks.asJava))
+
+    // TODO: produce a observable over the mouse click events
     val hbox = new HBox()
-    hbox.getChildren.addAll(
-      availableListView,
-      Views.trackListView(queuedTracks))
+    hbox.getChildren.addAll(availableListView, queuedListView)
 
     val vbox = new VBox()
     vbox.getChildren.addAll(
